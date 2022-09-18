afdc9e4b13c273de121ae44914573fb363cdc490
DrHistory <kursatkumsuz@gmail.com>
2022-09-18 17:44:33 +0300

    move to view

--- app/src/main/java/com/kursatkumsuz/marsrealestate/view/fragment/MarsFragmentFactory.kt
+++ app/src/main/java/com/kursatkumsuz/marsrealestate/view/fragment/MarsFragmentFactory.kt
@@ -1,4 +1,4 @@
-package com.kursatkumsuz.marsrealestate.fragment
+package com.kursatkumsuz.marsrealestate.view.fragment
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
