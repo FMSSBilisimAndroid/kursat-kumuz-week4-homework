279fd4338682c574559ff407a1300b9e17b7d9a4
DrHistory <kursatkumsuz@gmail.com>
2022-09-18 13:43:37 +0300

    update FragmentFactory

--- app/src/main/java/com/kursatkumsuz/marsrealestate/fragment/MarsFragmentFactory.kt
+++ app/src/main/java/com/kursatkumsuz/marsrealestate/fragment/MarsFragmentFactory.kt
@@ -3,17 +3,20 @@ package com.kursatkumsuz.marsrealestate.fragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
+import com.kursatkumsuz.marsrealestate.adapter.CartAdapter
 import com.kursatkumsuz.marsrealestate.adapter.MarsAdapter
 import javax.inject.Inject
 
 class MarsFragmentFactory @Inject constructor(
-    private val adapter: MarsAdapter,
+    private val listAdapter: MarsAdapter,
+    private val cartAdapter: CartAdapter,
     private val glide: RequestManager
 ) : FragmentFactory() {
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
-            FeedFragment::class.java.name -> FeedFragment(adapter)
+            FeedFragment::class.java.name -> FeedFragment(listAdapter)
             DetailFragment::class.java.name -> DetailFragment(glide)
+            CartFragment::class.java.name -> CartFragment(cartAdapter)
             else -> super.instantiate(classLoader, className)
         }
     }
